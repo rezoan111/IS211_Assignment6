646e5dca316e45a0ba4c6498f36528a849d8d951
rezoan111 <rezoan111@gmail.com>
2026-03-09 02:31:28 -0400

    conversions.py

--- conversions.py
+++ conversions.py
@@ -1,1 +1,10 @@
+# This function will convert Celsius to Kelvin.
+# Right now I am setting up the function first.
+def convertCelsiusToKelvin(celsius):
+    return 0.0
 
+
+# This function will convert Celsius to Fahrenheit.
+# Right now I am setting up the function first.
+def convertCelsiusToFahrenheit(celsius):
+    return 0.0
