a47bface3c7f841df669dae835c183749d57bde5
rezoan111 <rezoan111@gmail.com>
2026-03-09 02:34:37 -0400

    tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,18 @@
+import unittest
+import conversions
 
+
+# This class will hold my tests for the conversion functions.
+class ConversionTests(unittest.TestCase):
+
+    # This test will check Celsius to Kelvin conversions.
+    def test_convertCelsiusToKelvin(self):
+        pass
+
+    # This test will check Celsius to Fahrenheit conversions.
+    def test_convertCelsiusToFahrenheit(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
